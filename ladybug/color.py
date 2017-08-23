c157245caffb2730b80407e77f8b92db4bc99a87
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-08-23 03:18:33 -0400

    Added a check for colors with the same value

--- ladybug/color.py
+++ ladybug/color.py
@@ -458,7 +458,11 @@ class ColorRange(object):
         """Blend between two colors based on input value."""
         rangeMinP = self._domain[colorIndex]
         rangeP = self._domain[colorIndex + 1] - rangeMinP
-        factor = (value - rangeMinP) / rangeP
+        try:
+            factor = (value - rangeMinP) / rangeP
+        except ZeroDivisionError:
+            factor = 0
+
         minColor = self.colors[colorIndex]
         maxColor = self.colors[colorIndex + 1]
         red = round(factor * (maxColor.r - minColor.r) + minColor.r)
