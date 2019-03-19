3361688201358e668926b02f4dea166ec7f091d9
AntoineDao <antoinedao1@gmail.com>
2019-03-19 23:45:00 +0000

    fix(deploy): use javascript semantic-release package for deployment
    Using the "pure" javascript implementation of semantic-release makes it a bit more flexible to set
    up custom scripts to run at different stages of the semantic release process. The packages used as
    well as scripts to run at different stages are specified in the .releaserc.json file. In this case
    we use standard semantic-release and add a custom deploy script on top of the normal github release
    setup. The custom deploy script is the new 'deploy.sh' file which takes the new version number as an
    argument to deploy the package to PyPi and build the documentation.
    
    #125
    
    build(pytest): add pytest-cov deps to dev-requirements.txt
    
    build(euclid): add euclid to dev-requirements.txt

--- setup.py
+++ setup.py
@@ -5,29 +5,17 @@ import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('ladybug/__init__.py', 'r') as fd:
-    version = re.search(
-        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-        fd.read(),
-        re.MULTILINE
-    ).group(1)
-
-try:
-    from semantic_release import setup_hook
-    setup_hook(sys.argv)
-except ImportError:
-    pass
-
 setuptools.setup(
     name="lbt-ladybug",
-    version=version,
+    use_scm_version = True,
+    setup_requires=['setuptools_scm'],
     author="Ladybug Tools",
     author_email="info@ladybug.tools",
     description="Ladybug is a Python library to load, analyze and modify EneregyPlus Weather files (epw).",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ladybug-tools/ladybug",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=[
         'euclid3==0.1'
     ],
