45e3f283db941bed148321b08ca24ca62aeedd49
Chris Mackey <chris@ladybug.tools>
2021-02-19 21:05:33 -0500

    fix(docker): Setup auto-deployment to Dockerhub
    I am also adding a new option into the epw-to-wea command.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,20 @@
-FROM python:2.7
-MAINTAINER Ladybug Tools "info@ladybug.tools"
+FROM python:3.7
 
-WORKDIR /usr/local/lib/python2.7/site-packages
+LABEL maintainer="Ladybug Tools" email="info@ladybug.tools"
 
-# copy ladybug
-COPY ./ladybug ./ladybug
+# Create non-root user
+RUN adduser ladybugbot --uid 1000
+USER ladybugbot
+WORKDIR /home/ladybugbot
+RUN mkdir ladybug_tools && touch ladybug_tools/config.json
+
+
+# Install ladybug cli
+ENV PATH="/home/ladybugbot/.local/bin:${PATH}"
+COPY . ladybug
+RUN pip3 install setuptools wheel\
+    && pip3 install ./ladybug
+
+# Set up working directory
+RUN mkdir -p /home/ladybugbot/run/simulation
+WORKDIR /home/ladybugbot/run
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
+if [ -n "$1" ]
+then
+  NEXT_RELEASE_VERSION=$1
+else
+  echo "A release version must be supplied"
+  exit 1
+fi
+
+CONTAINER_NAME="ladybugtools/ladybug"
+
+echo "PyPi Deployment..."
 echo "Building distribution"
 python setup.py sdist bdist_wheel
 echo "Pushing new version to PyPi"
 twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD 
+
+
+echo "Docker Deployment..."
+echo "Login to Docker"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker build . -t $CONTAINER_NAME:$NEXT_RELEASE_VERSION 
+docker tag $CONTAINER_NAME:$NEXT_RELEASE_VERSION $CONTAINER_NAME:latest
+
+docker push $CONTAINER_NAME:latest
+docker push $CONTAINER_NAME:$NEXT_RELEASE_VERSION
