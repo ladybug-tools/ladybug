69b345e2952d83c53e4862fddd67b0e8b9b9ffa5
Chris Mackey <chris@ladybug.tools>
2021-02-24 17:54:02 -0500

    fix(cli): Interpret the use of empty string as a None input

--- ladybug/cli/_helper.py
+++ ladybug/cli/_helper.py
@@ -13,5 +13,6 @@ def _load_analysis_period_str(analysis_period_str):
     Args:
         analysis_period_str: A JSON file of an AnalysisPeriod to be loaded.
     """
-    if analysis_period_str is not None and analysis_period_str != 'None':
+    if analysis_period_str is not None and analysis_period_str != '' \
+            and analysis_period_str != 'None':
         return AnalysisPeriod.from_string(analysis_period_str)
